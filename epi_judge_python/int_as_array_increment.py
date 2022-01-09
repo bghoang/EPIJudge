168e2cb93fe3964590f252f699e263e0f04f7f6b
bghoang <bghoang@ucsd.edu>
2022-01-08 21:10:30 -0800

    5.2 and 5.17

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -5,7 +5,28 @@ from test_framework import generic_test
 
 def plus_one(A: List[int]) -> List[int]:
     # TODO - you fill in here.
-    return []
+    carry = 0
+    A.reverse()
+    for i in range(len(A)):
+        # print(A[i])
+        newVal = 0
+        # For the first value only
+        if i == 0:
+            newVal = A[i] + 1
+        else:
+            newVal = A[i] + carry
+            carry = 0
+
+        if newVal > 9:
+            A[i] = 0
+            carry = 1
+        else:
+            A[i] = newVal
+
+    if carry == 1:
+        A.append(carry)
+    A.reverse()
+    return A
 
 
 if __name__ == '__main__':
--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -6,6 +6,36 @@ from test_framework import generic_test
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
     # TODO - you fill in here.
+
+    # Checking each row for duplicate
+    for i in range(9):
+        d = dict()
+        for j in range(9):
+            if partial_assignment[i][j] != 0:
+                if partial_assignment[i][j] in d:
+                    return False
+                d[partial_assignment[i][j]] = 1
+
+    # Checking each columns for duplicate
+    for i in range(9):
+        d = dict()
+        for j in range(9):
+            if partial_assignment[j][i] != 0:
+                if partial_assignment[j][i] in d:
+                    return False
+                d[partial_assignment[j][i]] = 1
+
+    # Checking each 3x3 matrix
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            d = dict()
+            for x in range(i, i+3):
+                for y in range(j, j+3):
+                    if partial_assignment[x][y] != 0:
+                        if partial_assignment[x][y] in d:
+                            return False
+                        d[partial_assignment[x][y]] = 1
+
     return True
 
 
