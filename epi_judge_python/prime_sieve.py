804ef39def79fbb4dd98d417a837e8db14beea01
bghoang <bghoang@ucsd.edu>
2022-01-09 22:18:02 -0800

    5.5 and 5.9

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -6,7 +6,56 @@ from test_framework import generic_test
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
     # TODO - you fill in here.
-    return []
+    '''
+    BRUTE FORCE
+    Create an empty array to keep all the prime number called p
+    Add 2 to p because 2 is always a prime number
+    Loop i from 3 to n:
+        Go through each element in p
+            if i%an element in p == 0: continue
+        Add i to p because it is also a prime number
+
+
+    if n < 2:
+        return []
+    p = [2]
+    for i in range(3, n+1):
+        # print(p)
+        for num in p:
+            prime = False
+            if i % num == 0:
+                #print("Non prime: ", i)
+                # print("Check")
+                prime = True
+                break
+        if not prime:
+            p.append(i)
+    '''
+
+    '''
+    OPTIMAL
+    Create an array A with n+1 elements, set entry 0th and 1st to be 0 because they are not prime, set the rest to
+    be 1 because they are potentially a prime number
+    Loop through this array
+        If A[i] == 1, then add it to the prime list
+            Find all multiple of A[i] till it bigger than n, mark all the multiple to be 0 in array A
+    Return prime number list
+    '''
+    A = [0] * (n+1)
+    for i in range(len(A)):
+        if i >= 2:
+            A[i] = 1
+
+    p = []
+    for i in range(2, len(A)):
+        if A[i] == 1:
+            p.append(i)
+            # Update the multiple of A[i] in A to be 0
+            temp = i
+            while i*temp <= n:
+                A[i*temp] = 0
+                temp += 1
+    return p
 
 
 if __name__ == '__main__':
--- epi_judge_python/sorted_array_remove_dups.py
+++ epi_judge_python/sorted_array_remove_dups.py
@@ -8,7 +8,32 @@ from test_framework.test_utils import enable_executor_hook
 # Returns the number of valid entries after deletion.
 def delete_duplicates(A: List[int]) -> int:
     # TODO - you fill in here.
-    return 0
+    '''
+    Set p1 to be 0, p2 to be 1
+    Start a loop:
+        If A[p1] != A[p2]: increment p1 and p2
+        If A[p1] == A[p2]:
+            Do a while loop, keep increment p2 till A[p2] != A[p1] or p2 == len(A)
+            If p2 == len(A): then break the loop
+            Else: Increment p1 to 1 and set A[p1] = A[p2]
+    Return p1+1
+    '''
+    p1, p2 = 0, 1
+
+    while p2 < len(A):
+        if A[p1] != A[p2]:
+            p1 += 1
+            p2 += 1
+        else:
+            while p2 < len(A) and A[p1] == A[p2]:
+                p2 += 1
+            if p2 == len(A):
+                break
+            p1 += 1
+            A[p1] = A[p2]
+
+    # print(A)
+    return p1+1
 
 
 @enable_executor_hook
