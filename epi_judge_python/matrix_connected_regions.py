a09215ddf448f20a42e6d3a9601a555fd1073936
bghoang <bghoang@ucsd.edu>
2022-01-10 22:43:24 -0800

    18.1 and 18.2

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -5,7 +5,56 @@ from test_framework import generic_test
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
     # TODO - you fill in here.
+    '''
+    Do BFS iteratively, the reason is because we want to check for the color of the previous point.
+    Create a queue q
+    Created a visited list
+    Add (x,y) to the queue
+    Save old color at x,y
+
+    while q is not empty:
+        Pop the first element of the queue
+        Flip this color of this element
+        Mark as visited
+        Check the neighbor, check if they are within range 0<= x <= len(image) and 0<= y <= len(image[0])
+            if any of them we haven't visited and if any of them has the same oldColor then add them to the queue        
+    '''
+
+    q = []
+    visited = [[0 for _ in range(len(image[0]))] for _ in range(len(image))]
+    q.append((x, y))
+    oldColor = image[x][y]
+
+    while len(q) != 0:
+        currNode = q.pop(0)
+        # print(currNode)
+        #oldColor = image[currNode[0]][currNode[1]]
+        # print(oldColor)
+        image[currNode[0]][currNode[1]] = 1-oldColor
+        visited[currNode[0]][currNode[1]] = 1
+
+        n1 = (currNode[0]-1, currNode[1])
+        n2 = (currNode[0]+1, currNode[1])
+        n3 = (currNode[0], currNode[1]-1)
+        n4 = (currNode[0], currNode[1]+1)
+        maxR = len(image[0])
+        maxB = len(image)
+
+        if isValid(n1, maxR, maxB) and visited[n1[0]][n1[1]] == 0 and image[n1[0]][n1[1]] == oldColor:
+            q.append(n1)
+        if isValid(n2, maxR, maxB) and visited[n2[0]][n2[1]] == 0 and image[n2[0]][n2[1]] == oldColor:
+            q.append(n2)
+        if isValid(n3, maxR, maxB) and visited[n3[0]][n3[1]] == 0 and image[n3[0]][n3[1]] == oldColor:
+            q.append(n3)
+        if isValid(n4, maxR, maxB) and visited[n4[0]][n4[1]] == 0 and image[n4[0]][n4[1]] == oldColor:
+            q.append(n4)
     return
+
+
+def isValid(node, maxR, maxB):
+    if (node[0] >= 0 and node[0] < maxB and node[1] >= 0 and node[1] < maxR):
+        return True
+    return False
 
 
 def flip_color_wrapper(x, y, image):
--- epi_judge_python/search_maze.py
+++ epi_judge_python/search_maze.py
@@ -15,7 +15,55 @@ Coordinate = collections.namedtuple('Coordinate', ('x', 'y'))
 def search_maze(maze: List[List[int]], s: Coordinate,
                 e: Coordinate) -> List[Coordinate]:
     # TODO - you fill in here.
+    '''
+    Note: Recursive dfs don't need a stack 
+    Create a visited array
+    Create a stack
+    Call recursive DFS(maze, s, e, visited, stack)
+    Return stack
+
+    RecursiveDFS(maze, currNode, e, visited, stack):
+        if the currNode is not within the maze or not equal to WHITE: return False
+        if currNode is in visited: return False
+        add currNode to the stack
+        mark currNode as visited
+        if currNode == e then return True
+        call RecursiveDFS on all the currNode's neighbors
+        if can't find the path then remove the currNode from the stack and return False
+    '''
+    stack = []
+    visited = [[0 for _ in range(len(maze[0]))] for _ in range(len(maze))]
+    # Start DFS
+    if (recursiveDFS(maze, s, e, visited, stack)):
+        return stack
     return []
+
+
+def recursiveDFS(maze, currNode, e, visited, stack):
+    if not (currNode.x >= 0 and currNode.x < len(maze) and currNode.y >= 0 and currNode.y < len(maze[0])):
+        return False
+    if (maze[currNode.x][currNode.y] == BLACK or visited[currNode.x][currNode.y] == 1):
+        return False
+
+    stack.append(currNode)
+    visited[currNode.x][currNode.y] = 1
+    if (currNode.x == e.x and currNode.y == e.y):
+        return True
+
+    # Check neighbor
+    n1 = Coordinate(currNode.x-1, currNode.y)
+    n2 = Coordinate(currNode.x, currNode.y-1)
+    n3 = Coordinate(currNode.x+1, currNode.y)
+    n4 = Coordinate(currNode.x, currNode.y+1)
+
+    if (recursiveDFS(maze, n1, e, visited, stack) or
+        recursiveDFS(maze, n2, e, visited, stack) or
+        recursiveDFS(maze, n3, e, visited, stack) or
+            recursiveDFS(maze, n4, e, visited, stack)):
+        return True
+
+    stack.pop()
+    return False
 
 
 def path_element_is_feasible(maze, prev, cur):
@@ -23,12 +71,12 @@ def path_element_is_feasible(maze, prev, cur):
             (0 <= cur.y < len(maze[cur.x])) and maze[cur.x][cur.y] == WHITE):
         return False
     return cur == (prev.x + 1, prev.y) or \
-           cur == (prev.x - 1, prev.y) or \
-           cur == (prev.x, prev.y + 1) or \
-           cur == (prev.x, prev.y - 1)
+        cur == (prev.x - 1, prev.y) or \
+        cur == (prev.x, prev.y + 1) or \
+        cur == (prev.x, prev.y - 1)
 
 
-@enable_executor_hook
+@ enable_executor_hook
 def search_maze_wrapper(executor, maze, s, e):
     s = Coordinate(*s)
     e = Coordinate(*e)
