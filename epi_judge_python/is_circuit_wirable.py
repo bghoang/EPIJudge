2c9f25ddbd95f358261a04212006048d6216b083
bghoang <bghoang@ucsd.edu>
2022-01-16 17:32:52 -0800

    18.6

--- epi_judge_python/is_circuit_wirable.py
+++ epi_judge_python/is_circuit_wirable.py
@@ -13,6 +13,38 @@ class GraphVertex:
 
 def is_any_placement_feasible(graph: List[GraphVertex]) -> bool:
     # TODO - you fill in here.
+    '''
+    This is checking if a graph is bipartie, meaning that for a graph, split the vertices into 2 sets
+    there is no connection between 2 verices that are in the same set, only connection between 2 nodes from different sets
+
+    Using BFS approach, keep track of the distance from each node to its neighbor
+    We stop and return False if we encounter a neighbor node that has the same distance as the current node 
+
+    Another way of doing this is to assign each node to a color, while doing bfs, if we encounter neighbor node with the same color as the current node then we return false
+
+    '''
+
+    for v in graph:
+        if v.d == -1:
+            check = bfs(v)
+            if check == False:
+                return False
+    return True
+
+
+def bfs(node):
+    q = []
+    q.append(node)
+
+    while len(q) > 0:
+        currNode = q.pop(0)
+
+        for neighbor in currNode.edges:
+            if neighbor.d == -1:  # If this neighbor is not visited yet
+                neighbor.d = currNode.d + 1
+                q.append(neighbor)
+            elif neighbor.d == currNode.d:
+                return False
     return True
 
 
