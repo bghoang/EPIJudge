152c957e4cdfee2b424b34fb5d109d3a89fab8e8
bghoang <bghoang@ucsd.edu>
2022-01-18 20:56:14 -0800

    6.1 and 6.2

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -3,7 +3,40 @@ from test_framework import generic_test
 
 def convert_base(num_as_string: str, b1: int, b2: int) -> str:
     # TODO - you fill in here.
-    return ''
+    d = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+    d1 = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+    res = ""
+    temp = 0
+    base = 1
+    end = -1
+    neg = 0
+    if num_as_string[0] == '-':
+        end = 0
+        neg = 1
+    for i in range(len(num_as_string)-1, end, -1):
+        if num_as_string[i] in d1:
+            digit = int(d1[num_as_string[i]]) * base
+        else:
+            digit = int(num_as_string[i]) * base
+        temp += digit
+        base *= b1
+
+    while True:
+        rem = temp % b2
+        temp = temp//b2
+
+        if rem in d:
+            res += d[rem]
+        else:
+            res += str(rem)
+
+        # Breakd if temp get to 0
+        if temp == 0:
+            break
+
+    if neg:
+        return '-' + res[::-1]
+    return res[::-1]
 
 
 if __name__ == '__main__':
--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,50 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
+import functools
+import string
 
 
 def int_to_string(x: int) -> str:
     # TODO - you fill in here.
-    return '0'
+    res = []
+    neg = 0
+    if x < 0:
+        neg = 1
+        x = x * -1
+    while True:
+        lastDigit = x % 10
+        res.append(chr(ord('0') + lastDigit))
+        x = x//10
+        if x == 0:
+            break
+    if neg:
+        return "-" + "".join(reversed(res))
+    return "".join(reversed(res))
 
 
 def string_to_int(s: str) -> int:
     # TODO - you fill in here.
-    return 0
+    res = 0
+    neg = 0
+    start = 0
+    if s[0] == "-":
+        neg = 1
+        start = 1
+    elif s[0] == "+":
+        start = 1
+    for i in range(start, len(s)):
+        temp = int(s[i])
+        res += temp
+        if i != len(s)-1:
+            res = res * 10
+
+    if neg:
+        return 0 - res
+    return res
+    '''
+    return (-1 if s[0] == '-' else 1) * functools.reduce(
+        lambda running_sum, c: running_sum * 10 + string.digits.index(c),
+        s[s[0] in '-+':], 0)'''
 
 
 def wrapper(x, s):
