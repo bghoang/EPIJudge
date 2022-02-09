68ccfd55b264691438475ce75408ed24e153b740
bghoang <bghoang@ucsd.edu>
2022-02-08 19:39:07 -0800

    15.4 15.5

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -5,7 +5,23 @@ from test_framework import generic_test, test_utils
 
 def permutations(A: List[int]) -> List[List[int]]:
     # TODO - you fill in here.
-    return []
+    '''
+    Backtracking with recursion, if a number is in current permutation then skip, else add it to permutation
+    If permutation len is equal to len of given list and add to result. 
+    '''
+    res = []
+    backtrack(res, [], A)
+    return res
+
+
+def backtrack(res, currPer, A):
+    if len(currPer) == len(A):
+        res.append(currPer)
+        return
+
+    for i in range(len(A)):
+        if A[i] not in currPer:
+            backtrack(res, currPer + [A[i]], A)
 
 
 if __name__ == '__main__':
--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -5,7 +5,23 @@ from test_framework import generic_test, test_utils
 
 def generate_power_set(input_set: List[int]) -> List[List[int]]:
     # TODO - you fill in here.
-    return []
+    if len(input_set) == 0:
+        return [[]]
+    res = []
+    backtrack(0, [], res, input_set)
+
+    return res
+
+
+def backtrack(currInd, currSet, res, input_set):
+    if len(currSet) <= len(input_set):
+        res.append(currSet)
+    else:
+        return
+    # print(currSet)
+
+    for i in range(currInd, len(input_set)):
+        backtrack(i+1, currSet + [input_set[i]], res, input_set)
 
 
 if __name__ == '__main__':
